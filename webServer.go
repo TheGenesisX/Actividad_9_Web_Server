b637b1fa29b971f706a898e002554831f13444db
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-11-29 22:28:50 -0600

    Modificacion de la estructura de los maps, que dificultaba su iteracion. Modificacion de toda la funcion de registro de calificaciones porque tenia problemas de registro correcto. Se saltaba datos o los eliminaba por falta de validaciones. Que buen tiro me pegue arreglando esto. Me tomó como 3 horas.

--- webServer.go
+++ webServer.go
@@ -7,27 +7,31 @@ import (
 	"strconv"
 )
 
-var materias = map[string]informacion{}
-var alumnos = map[string]informacion{}
+var materias = map[string]map[string]float64{}
+var alumnos = map[string]map[string]float64{}
 
-type informacion struct {
-	nombre       string // Nombre de materia o alumno, dependiendo del map.
-	calificacion float64
-}
+// var materias = map[string]informacion{}
+// var alumnos = map[string]informacion{}
 
-func obtenerPromedioIndividual(nombreAlumno string) float64 {
-	promedio, contadorMaterias := 0.0, 0.0
+// type informacion struct {
+// 	nombre       string // Nombre de materia o alumno, dependiendo del map.
+// 	calificacion float64
+// }
 
-	for _, value := range alumnos {
-		if value.nombre == nombreAlumno {
-			promedio += value.calificacion
-			contadorMaterias++
-		}
-	}
-	promedio /= contadorMaterias
-	return promedio
+// func obtenerPromedioIndividual(nombreAlumno string) float64 {
+// 	promedio, contadorMaterias := 0.0, 0.0
 
-}
+// 	for _, value := range alumnos {
+// 		fmt.Println(value)
+// 		if value.nombre == nombreAlumno {
+// 			promedio += value.calificacion
+// 			contadorMaterias++
+// 		}
+// 	}
+// 	promedio /= contadorMaterias
+// 	return promedio
+
+// }
 
 func loadHTML(htmlDocument string) string {
 	html, _ := ioutil.ReadFile(htmlDocument)
@@ -63,14 +67,43 @@ func postReceiver(response http.ResponseWriter, request *http.Request) {
 
 		// Convertimos el string de la calificacion del form en float.
 		if calificacion, err := strconv.ParseFloat(aux, 32); err == nil {
-			alumnos[alumno] = informacion{nombre: materia, calificacion: calificacion}
-			materias[materia] = informacion{nombre: alumno, calificacion: calificacion}
-			message = "Registro realizado con exito"
+			if _, alumnoExists := alumnos[alumno]; alumnoExists {
+				if _, materiaExists := alumnos[alumno][materia]; materiaExists {
+					message = "Calificacion existente. Imposible modificar."
+					// Si ambos existen, no podemos modificar la calificacion existente.
+				} else {
+					alumnos[alumno][materia] = calificacion
 
+					materias[materia] = make(map[string]float64)
+					materias[materia][alumno] = calificacion
+					message = "Registro realizado con exito"
+					// Si existe el alumno pero no la materia, la creamos y registramos.
+				}
+			} else {
+				// Si no existe el alumno, necesitamos revisar si existe o no la materia.
+				if _, materiaExists := materias[materia]; materiaExists {
+					alumnos[alumno] = make(map[string]float64)
+					alumnos[alumno][materia] = calificacion
+
+					materias[materia][alumno] = calificacion
+					message = "Registro realizado con exito"
+				} else {
+					// Si no existen ninguno de los dos, los creamos y les damos sus valores.
+					alumnos[alumno] = make(map[string]float64)
+					alumnos[alumno][materia] = calificacion
+
+					materias[materia] = make(map[string]float64)
+					materias[materia][alumno] = calificacion
+					message = "Registro realizado con exito"
+				}
+			}
 		} else {
 			fmt.Println(err)
 			return
 		}
+
+		fmt.Println("Alumnos:", alumnos)
+		fmt.Println("Materias:", materias)
 
 		response.Header().Set(
 			"Content-Type",
@@ -85,42 +118,42 @@ func postReceiver(response http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func promedioIndividual(response http.ResponseWriter, request *http.Request) {
-	var message string
+// func promedioIndividual(response http.ResponseWriter, request *http.Request) {
+// 	var message string
 
-	switch request.Method {
-	case "GET":
-		if err := request.ParseForm(); err != nil {
-			fmt.Fprintf(response, "ParseForm() error %v", err)
-			return
-		}
+// 	switch request.Method {
+// 	case "GET":
+// 		if err := request.ParseForm(); err != nil {
+// 			fmt.Fprintf(response, "ParseForm() error %v", err)
+// 			return
+// 		}
 
-		alumno := request.FormValue("promedioIndividual")
-		promedio := obtenerPromedioIndividual(alumno)
+// 		alumno := request.FormValue("promedioIndividual")
+// 		promedio := obtenerPromedioIndividual(alumno)
 
-		if promedio == -1 {
-			message = "Alumno no existente"
-		} else {
-			message = fmt.Sprintf("%f", promedio)
-		}
+// 		if promedio == -1 {
+// 			message = "Alumno no existente"
+// 		} else {
+// 			message = fmt.Sprintf("%f", promedio)
+// 		}
 
-		response.Header().Set(
-			"Content-Type",
-			"text.html",
-		)
+// 		response.Header().Set(
+// 			"Content-Type",
+// 			"text.html",
+// 		)
 
-		fmt.Fprintf(
-			response,
-			loadHTML("promedioIndividual.html"),
-			message,
-		)
-	}
-}
+// 		fmt.Fprintf(
+// 			response,
+// 			loadHTML("promedioIndividual.html"),
+// 			message,
+// 		)
+// 	}
+// }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/postReceiver", postReceiver)
-	http.HandleFunc("/promedioIndividual", promedioIndividual)
+	// http.HandleFunc("/promedioIndividual", promedioIndividual)
 	fmt.Println("Servidor en ejecucion...")
 	http.ListenAndServe(":9000", nil)
 }
